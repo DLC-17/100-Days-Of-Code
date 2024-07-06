cdd8639d9343955a92d902b2c686bec2f2a16c3a
D C <84093031+DLC-17@users.noreply.github.com>
2024-07-06 12:43:02 -0700

    Day Seven python program a hangman game

---
+++ DayTwo-Interactive Lessons/DayTwo-Digit Converter.py
@@ -0,0 +1,8 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+sum=0
+for i in two_digit_number:
+  sum+=int(i)
+print(sum)
